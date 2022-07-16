d69dd5b15423145ffd60dc7d6f37a11dbdf9bd5c
Cihat <chtakyol@gmail.com>
2022-07-16 16:09:25 +0300

    Dev (#2)
    * Some game logic, letter box and utilities.
    
    * Different letterbox idea implemented and some view model features are added.

--- app/src/main/java/com/oolong/dictionary_quiz/util/Utils.kt
+++ app/src/main/java/com/oolong/dictionary_quiz/util/Utils.kt
@@ -1,7 +1,7 @@
 package com.oolong.dictionary_quiz.util
 
 object Utils {
-    val letters = listOf("a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "l", "m", "n", "o", "p")
+    val letters = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
     enum class AnswerState { PASSIVE, IDLE, TRUE, FALSE, PASS }
 }
 
